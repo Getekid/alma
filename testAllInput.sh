a19901bcc5ac40df6a415eee2953f1d246549f80
Thomas Pappas <getekid@gmail.com>
2019-11-25 01:38:34 +0200

    Algorithms: Added a bash scrip to run a c++ compiled executable with all given inputs
    Provided there is a folder with 'input#.txt' and 'output#.txt' files

---
+++ testAllInput.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ "$1" == "" ] || [ "$2" == "" ];
+then
+    echo "Please provide the script to run and the folder with the input/output scripts"
+    exit 2
+fi
+
+for i in $(seq 1 20);
+do
+    input="$2/input$i.txt"
+    expected=$(cat $2/output$i.txt)
+    echo "Running script with input $input, expected output is $expected"
+    output=$(/usr/bin/time -f "%e %M" ./$1 < $input)
+    if [ "$output" == "$expected" ];
+    then
+        echo "SUCCESS: Input $i returns the expected output!"
+    else
+        echo "ERROR: Input $i does NOT return the expected output! Output: $output"
+    fi
+    if (( $i >= 10 ));
+    then
+        exit 2
+    fi
+done
