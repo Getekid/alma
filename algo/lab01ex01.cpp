5168f48657c7a4143877eec9b821f21740491eae
Thomas Pappas <getekid@gmail.com>
2019-11-26 03:30:23 +0200

    Algorithms: lab01ex01: Used binary search to find the min fuel for a valid car and used that to filter through the cars
    Also fixed a few typos

--- algo/lab01ex01.cpp
+++ algo/lab01ex01.cpp
@@ -16,12 +16,14 @@ class roadtrip
       int ts, cs, tf, cf;
       void getInput();
       // Helper.
+      int fuelMin = 0, fuelMax = 0;
       vector<int> stationDistDiff; // A vector to store the diff distance between the stations.
       int getFuelDistanceMinTime(int, int);
-      bool carIsValid(array<int, 2>);
+      bool fuelIsValid(int);
+      int calculateMinFuelSuccess(int, int);
       // Output.
       int minRent = -1;
-      int caclulateMinRent();
+      int calculateMinRent();
 };
 
 void roadtrip::getInput()
@@ -37,6 +39,8 @@ void roadtrip::getInput()
         array<int, 2> specs;
         cin >> specs[0] >> specs[1];
         cars.push(specs);
+        fuelMin = (fuelMin == 0 || fuelMin > specs[1]) ? specs[1] : fuelMin;
+        fuelMax = (fuelMin == 0 || fuelMax < specs[1]) ? specs[1] : fuelMax;
     }
 
     // Get the station distance data.
@@ -80,7 +84,7 @@ int roadtrip::getFuelDistanceMinTime(int fuel, int distance)
         return -1;
     }
 
-    // Calcullate the minimum distance x in economic functionality.
+    // Calculate the minimum distance x in economic functionality.
     // If x is bigger than the distance then the car cannot cross, return -1.
     int x = (distXcf - fuel)/(cf - cs);
     x = (x < 0) ? 0 : x;
@@ -94,7 +98,7 @@ int roadtrip::getFuelDistanceMinTime(int fuel, int distance)
     }
 }
 
-bool roadtrip::carIsValid(array<int, 2> car)
+bool roadtrip::fuelIsValid(int fuel)
 {
     int timePassed = 0;
     int pastResult[2] = {0, 0}; // A variable to avoid calculating again the same distances.
@@ -103,7 +107,6 @@ bool roadtrip::carIsValid(array<int, 2> car)
     for (size_t i = 0; i < numStations + 1; i++)
     {
         int timeNeeded;
-        // int distToStation = stationDist[i] - distMoved;
         int distToStation = stationDistDiff[i];
         if (pastResult[0] == distToStation)
         {
@@ -111,7 +114,7 @@ bool roadtrip::carIsValid(array<int, 2> car)
         }
         else
         {
-            timeNeeded = getFuelDistanceMinTime(car[1], distToStation);
+            timeNeeded = getFuelDistanceMinTime(fuel, distToStation);
             if (timeNeeded < 0)
             {
                 return false;
@@ -130,11 +133,35 @@ bool roadtrip::carIsValid(array<int, 2> car)
     return true;
 }
 
-int roadtrip::caclulateMinRent()
+int roadtrip::calculateMinFuelSuccess(int min, int max)
 {
-    int fuelFail = 0;
-    unsigned long int fuelSuccess = dist * cf;
+    if (max - min == 1)
+    {
+        return fuelIsValid(min) ? min : (fuelIsValid(max) ? max : -1);
+    }
+
+    if (!fuelIsValid(max))
+    {
+        return -1;
+    }
+    if (fuelIsValid(min))
+    {
+        return min;
+    }
+
+    // Binary search: Check the media value and recurse accordingly.
+    int median = ((min + max) / 2);
+    return (!fuelIsValid(median)) ? calculateMinFuelSuccess(median, max) : calculateMinFuelSuccess(min, median);
+}
+
+int roadtrip::calculateMinRent()
+{
     int minRentLocal = 1000000000;
+    int minFuelSuccess = calculateMinFuelSuccess(fuelMin, fuelMax);
+    if (minFuelSuccess < 0)
+    {
+        return -1;
+    }
     bool hasValidCar = false;
     while(!cars.empty())
     {
@@ -145,27 +172,13 @@ int roadtrip::caclulateMinRent()
         // - cost higher that the one of a valid car or
         // - fuel capacity is lower that one of an invalid car
         // then this car is invalid.
-        if ((hasValidCar && (car[0] >= minRentLocal)) || car[1] <= fuelFail)
+        if ((hasValidCar && (car[0] >= minRentLocal)) || car[1] < minFuelSuccess)
         {
             continue;
         }
-        
-        if (car[1] >= fuelSuccess || carIsValid(car))
-        {
-            hasValidCar = true;
-            if (minRentLocal > car[0])
-            {
-                minRentLocal = car[0];
-            }
-            if (fuelSuccess > car[1])
-            {
-                fuelSuccess = car[1];
-            }
-        }
-        else
-        {
-            fuelFail = car[1];
-        }
+
+        hasValidCar = true;
+        minRentLocal = (minRentLocal > car[0]) ? car[0] : minRentLocal;
     }
     if (hasValidCar)
     {
@@ -179,7 +192,7 @@ int main()
 {
     roadtrip rt;
     rt.getInput();
-    cout << rt.caclulateMinRent() << "\n";
+    cout << rt.calculateMinRent() << "\n";
 
     return 0;
 }
