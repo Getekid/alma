139487a6130cd45f56903892aebd61a3c3fb10db
Thomas Pappas <getekid@gmail.com>
2020-01-10 04:57:19 +0200

    Algorithms: lab02ex01: Altered the algorithm for 1 exception to use class variables and implemented case for K > 1 (but very slow)

--- algo/lab02ex01.cpp
+++ algo/lab02ex01.cpp
@@ -3,13 +3,35 @@
 using namespace std;
 
 class kcredits {
+    int *maxLengthDataIncr;
+    int **maxLengthDataDecr;
     static int binSearch(vector<int>, int, bool);
 public:
+    kcredits();
     int N, K;
-    vector<int> persons;
-    void getInput();
-    static int getMaxWithOneException(vector<int>);
+    vector<int> personCredit;
+    int getMaxWithOneException(int);
+    int getMax();
 };
+
+kcredits::kcredits() {
+    // Get the number of the intersections and the minimum number of streets to light.
+    cin >> N >> K;
+
+    // Initialise the lengthData vectors.
+    maxLengthDataIncr = new int [N]{1};
+    maxLengthDataDecr = new int* [N];
+    for (int i = 0; i < N; i++) {
+        maxLengthDataDecr[i] = new int[N];
+        maxLengthDataDecr[i][i] = 1;
+    }
+
+    for (int i = 1; i <= N; i++) {
+        int personCr;
+        cin >> personCr;
+        personCredit.push_back(personCr);
+    }
+}
 
 int kcredits::binSearch(vector<int> v, int key, bool isIncreasing = true) {
     int l = -1;
@@ -33,30 +55,29 @@ int kcredits::binSearch(vector<int> v, int key, bool isIncreasing = true) {
     return r;
 }
 
-int kcredits::getMaxWithOneException(vector<int> seq) {
-    int n = seq.size();
-
+int kcredits::getMaxWithOneException(int end) {
     // Do a LIS for the sequence.
     vector<int> tailI;
     int length = 1;
-    // Keep the max length up until each element.
-    vector<int> subMaxI(n, 1);
 
-    // Initialise for the first element.
-    tailI.push_back(seq[0]);
-    for (int i = 1; i < n; i++) {
-        if (seq[i] < tailI[0]){
-            tailI[0] = seq[i];
-            subMaxI[i] = subMaxI[i-1];
-        }
-        else if (seq[i] > tailI[length - 1]) {
-            tailI.push_back(seq[i]);
-            length++;
-            subMaxI[i] = length;
-        }
-        else {
-            tailI[binSearch(tailI, seq[i])] = seq[i];
-            subMaxI[i] = subMaxI[i-1];
+    // Quick fix: Only run the LIS when we're looking for the whole size.
+    if (end == N-1) {
+        // Initialise for the first element.
+        tailI.push_back(personCredit[0]);
+        for (int i = 1; i <= end; i++) {
+            if (personCredit[i] < tailI[0]){
+                tailI[0] = personCredit[i];
+                maxLengthDataIncr[i] = maxLengthDataIncr[i-1];
+            }
+            else if (personCredit[i] > tailI[length - 1]) {
+                tailI.push_back(personCredit[i]);
+                length++;
+                maxLengthDataIncr[i] = length;
+            }
+            else {
+                tailI[binSearch(tailI, personCredit[i])] = personCredit[i];
+                maxLengthDataIncr[i] = maxLengthDataIncr[i-1];
+            }
         }
     }
 
@@ -64,56 +85,69 @@ int kcredits::getMaxWithOneException(vector<int> seq) {
     // We look for the LDS instead from the inverted (in the respect of order of transversing) vector.
     vector<int> tailD;
     length = 1;
-    // Keep the max length up until each element.
-    vector<int> subMaxD(n, 1);
 
     // Initialise for the first element.
-    tailD.push_back(seq[n-1]);
-    for (int i = n-2; i >= 0; i--) {
-        if (seq[i] > tailD[0]){
-            tailD[0] = seq[i];
-            subMaxD[i] = subMaxD[i+1];
+    tailD.push_back(personCredit[end]);
+    for (int i = end-1; i >= 0; i--) {
+        if (personCredit[i] > tailD[0]){
+            tailD[0] = personCredit[i];
+            maxLengthDataDecr[end][i] = maxLengthDataDecr[end][i+1];
         }
-        else if (seq[i] < tailD[length - 1]) {
-            tailD.push_back(seq[i]);
+        else if (personCredit[i] < tailD[length - 1]) {
+            tailD.push_back(personCredit[i]);
             length++;
-            subMaxD[i] = length;
+            maxLengthDataDecr[end][i] = length;
         }
         else {
-            tailD[binSearch(tailD, seq[i], false)] = seq[i];
-            subMaxD[i] = subMaxD[i+1];
-
+            tailD[binSearch(tailD, personCredit[i], false)] = personCredit[i];
+            maxLengthDataDecr[end][i] = maxLengthDataDecr[end][i+1];
         }
     }
 
     // Find the max sum between the two max value vectors.
-    int max = subMaxD[0]; // Which is the same as subMaxI[n-1].
-    for (int i = 0; i < n-2; i++) {
-        if (max < subMaxI[i] + subMaxD[i+1])
-            max = subMaxI[i] + subMaxD[i+1];
+    int max = maxLengthDataIncr[end]; // Which is the same as maxLengthDataDecr[end][0].
+    for (int i = 1; i <= end; i++) {
+        if (max < maxLengthDataIncr[i-1] + maxLengthDataDecr[end][i])
+            max = maxLengthDataIncr[i-1] + maxLengthDataDecr[end][i];
     }
 
     return max;
 }
 
-void kcredits::getInput() {
-    // Get the number of the intersections and the minimum number of streets to light.
-    cin >> N >> K;
-
-    for (int i = 1; i <= N; i++) {
-        int person;
-        cin >> person;
-        persons.push_back(person);
+int kcredits::getMax() {
+    int maxCreditData[N][K];
+    // Set the initial values.
+    for (int k = 0; k < K; k++)
+        maxCreditData[0][k] = 1;
+    for (int n = N-1; n >= 0; n--) {
+        maxCreditData[n][0] = getMaxWithOneException(n);
     }
+    // Now for the recursion.
+    for (int n = 1; n < N; n++) {
+        for (int k = 1; k < K; k++) {
+            if (n <= k)
+                maxCreditData[n][k] = n+1;
+            else {
+                int subMax = k;
+                for (int i = k; i <= n; i++) {
+                    int subMaxK = maxCreditData[i-1][k-1] + maxLengthDataDecr[n][i];
+                    if (subMax < subMaxK)
+                        subMax = subMaxK;
+                }
+                maxCreditData[n][k] = subMax;
+            }
+        }
+    }
+
+    return maxCreditData[N-1][K-1];
 }
 
 int main() {
     kcredits kc;
-    kc.getInput();
     if (kc.K == 1)
-        cout << kc.getMaxWithOneException(kc.persons) << "\n";
+        cout << kc.getMaxWithOneException(kc.N-1) << "\n";
     else
-        cout << 0;
+        cout << kc.getMax();
 
     return 0;
 };
