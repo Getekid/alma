b4ac8bd78b29a0fcd8c8bd208951a289823eaa4a
Thomas Pappas <getekid@gmail.com>
2020-01-31 23:07:28 +0200

    Algorithms: lab03ex01: Improvements to printing, Union/Find and cycle check, plus code improvements

--- algo/lab03ex01.cpp
+++ algo/lab03ex01.cpp
@@ -4,18 +4,17 @@
 
 using namespace std;
 
-/*
- * Union/Find algorithm by GeeksforGeeks.
- * @see https://www.geeksforgeeks.org/union-find/
- */
-// A utility function to find the subset of an element i
 int find(int parent[], int i)
 {
-    if (parent[i] == -1)
-        return i;
-    return find(parent, parent[i]);
+    while (parent[i] != -1)
+        i = parent[i];
+    return i;
 }
 
+/*
+ * Union algorithm by GeeksforGeeks.
+ * @see https://www.geeksforgeeks.org/union-find/
+ */
 // A utility function to do union of two subsets
 void Union(int parent[], int x, int y)
 {
@@ -36,12 +35,12 @@ public:
     int *spEdges;
     // Helper functions and variables.
     int *parent;
-    bool isCycle(vector<pair<int, int>>);
+    int *ufSubsets;
+    bool hasCycle(pair<int, int>*);
     void buildMST();
     vector<int> dfs_init(int);
     void dfs(bool*, int, int*);
-    void calculateSPs();
-    int getBinarySPs();
+    void printBinarySPs();
 };
 
 distances::distances() {
@@ -49,37 +48,34 @@ distances::distances() {
 
     edges = new pair<int, int>[M];
     mst = new map<int, int>[N];
-    parent = new int[N];
     spEdges = new int[M];
-    for (int i = 0; i < M; i++) {
+    parent = new int[N];
+    ufSubsets = new int[N];
+    for (int m = 0; m < M; m++) {
         int a, b, c;
         cin >> a >> b >> c;
         edges[c] = make_pair(a, b);
     }
+    for (int n = 0; n < N; n++)
+        ufSubsets[n] = -1;
 }
 
-bool distances::isCycle(vector<pair<int, int>> mstEdges) {
-    int subsets[N];
-    for (int i = 0; i < N; i++) {
-        subsets[i] = -1;
-    }
+bool distances::hasCycle(pair<int, int> *candEdge) {
+    int subsetSource = find(ufSubsets, candEdge->first);
+    int subsetDest = find(ufSubsets, candEdge->second);
+    if (subsetSource == subsetDest)
+        return true;
+    // Union.
+    ufSubsets[subsetSource] = subsetDest;
 
-    for (int i = 0; i < mstEdges.size(); i++) {
-        int subsetSource = find(subsets, mstEdges[i].first - 1);
-        int subsetDest = find(subsets, mstEdges[i].second - 1);
-        if (subsetSource == subsetDest)
-            return true;
-        Union(subsets, subsetSource, subsetDest);
-    }
     return false;
 }
 
 void distances::buildMST() {
-    vector<pair<int, int>> mstEdges;
     for (int i = 0; i < M; i++) {
-        mstEdges.emplace_back(edges[i].first, edges[i].second);
-        if (isCycle(mstEdges)) {
-            mstEdges.pop_back();
+        pair<int, int> candEdge(edges[i].first - 1, edges[i].second - 1);
+        if (hasCycle(&candEdge)) {
+            continue;
         } else {
             // Also add the Edge to the mst array to use in the later DFS.
             mst[edges[i].first - 1].insert(make_pair(edges[i].second - 1, i));
@@ -113,16 +109,8 @@ vector<int> distances::dfs_init(int s) {
     return dfsOrder;
 }
 
-//void distances::calculateSPs() {
-//    vector<int> dfsOrder = dfs_init(0);
-//    cout << dfsOrder.size();
-////    for (int i = 0; i < N; i++) {
-////        cout << dfsOrder[i] << endl;
-////    }
-//}
-
-int distances::getBinarySPs() {
-    int binSum = 0;
+void distances::printBinarySPs() {
+    bool startPrint = false;
     for (int i = 0; i < M-1; i++) {
         int edge = spEdges[i];
         spEdges[i] = edge % 2;
@@ -130,24 +118,33 @@ int distances::getBinarySPs() {
     }
 
     // Special handling for the last element.
-    string binEdge;
-    while (spEdges[M-1] >= 2) {
-        binEdge += to_string(spEdges[M-1] % 2);
-        spEdges[M-1] = spEdges[M-1] / 2;
+    if (spEdges[M-1] != 0) {
+        startPrint = true;
+        string binEdge;
+        while (spEdges[M-1] >= 2) {
+            binEdge += to_string(spEdges[M-1] % 2);
+            spEdges[M-1] = spEdges[M-1] / 2;
+        }
+        cout << spEdges[M-1] << binEdge;
     }
-    cout << spEdges[M-1] << binEdge;
     for (int i = M-2; i >= 0; i--)
-        cout << spEdges[i];
+        if (!startPrint && spEdges[i] == 0)
+            continue;
+        else {
+            startPrint = true;
+            cout << spEdges[i];
+        }
+    // If the printing still hasn't started, print 0.
+    if (!startPrint)
+        cout << 0;
     cout << endl;
-    return binSum;
 }
 
 int main() {
     distances dist;
     dist.buildMST();
     dist.dfs_init(0);
-    dist.getBinarySPs();
-//    cout << dist.getBinarySPs() << endl;
+    dist.printBinarySPs();
 
     return 0;
 }
