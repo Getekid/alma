a0f829bca8e47cc15ef6bdcff6425f13ed49aaa1
Thomas Pappas <getekid@gmail.com>
2020-03-14 12:39:29 +0200

    Algorithms: lab04ex02: Included recursive getMaxSide method to calculate the max degree of each country
    Calculating both the degree for each side and the overall sum of them, at the current state only a path inside a single planet is correct as it doesn't check different planets relatively

--- algo/lab04ex02.cpp
+++ algo/lab04ex02.cpp
@@ -9,6 +9,8 @@ public:
     int maxPathInner = 0;
     vector<vector<int>> *countries;
     ring();
+    int getMaxSide(int k, vector<int> root);
+    int getMaxPath();
 };
 
 ring::ring() {
@@ -28,8 +30,45 @@ ring::ring() {
     }
 }
 
+int ring::getMaxSide(int k, vector<int> root) {
+    // Helper code to print stuff. - Still to make Debug work...
+    // cout << "Children(" << root.size() << "): " << (root.empty() ? 0 : root[0]) << ", " << ((root.size() == 2) ? root[1] : 0) << endl;
+    // At each step, also check for the combined path (aka the inner planet path.
+    int degree, degreeLeft, degreeRight;
+    switch (root.size()) {
+        case 0:
+            return 0;
+        case 1:
+            degree = getMaxSide(k, countries[k][root[0]]) + 1;
+            if (maxPathInner < degree)
+                maxPathInner = degree;
+            return degree;
+        case 2:
+            degreeLeft = getMaxSide(k, countries[k][root[0]]) + 1;
+            degreeRight = getMaxSide(k, countries[k][root[1]]) + 1;
+            if (maxPathInner < degreeLeft + degreeRight)
+                maxPathInner = degreeLeft + degreeRight;
+            return max(degreeLeft, degreeRight);
+    }
+
+    return -1;
+}
+
+int ring::getMaxPath() {
+    int maxPathSide = 0;
+    for (int k = 0; k < K; k++) {
+        int pathSide = getMaxSide(k, countries[k][0]);
+        if (maxPathSide < pathSide)
+            maxPathSide = pathSide;
+        // Helper code to print stuff. - Still to make Debug work...
+        // cout << "Planet: " << k << " - Max Side Path: " << pathSide << endl;
+    }
+    return max(maxPathInner, maxPathSide);
+}
+
 int main() {
     ring r;
+    cout << r.getMaxPath() << endl;
 
      // Helper code to print stuff. - Still to make Debug work...
 //    for (int k = 0; k < r.K; k++) {
